83126967ba776b90c41c17b6c39a69b3a6056ef5
PedroRossa98 <64550870+PedroRossa98@users.noreply.github.com>
2021-11-16 23:07:18 +0000

    Update RPi_Tester.py

--- RPi_Tester.py
+++ RPi_Tester.py
@@ -113,6 +113,6 @@ def getPoint(execution_id,result_id):
         return ''
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 5005,debug=True)
+    app.run(host="0.0.0.0", port = 5000,debug=True)
 
 
